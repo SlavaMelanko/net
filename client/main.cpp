13ac8f691c969787c4a3e04ae277756f1d1e828e
smela <slava.melanko@gmail.com>
2020-03-03 21:05:36 +0200

    Tested dealer/router pattern

--- client/main.cpp
+++ client/main.cpp
@@ -1,10 +1,10 @@
 #include <zhelpers.hpp>
-
+#if 0
 int main()
 {
     try {
         zmq::context_t context{1};
-        zmq::socket_t subscriber(context, ZMQ_SUB);
+        zmq::socket_t subscriber{context, ZMQ_SUB};
         subscriber.connect("tcp://127.0.0.1:5555");
         subscriber.setsockopt(ZMQ_SUBSCRIBE, "A", 1);
 
@@ -14,6 +14,39 @@ int main()
             std::clog << "Receiving content..." << std::endl;
             const auto content = s_recv(subscriber);
             std::clog << topic << ": " << content << std::endl;
+        }
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
+#endif
+int main()
+{
+    try {
+        srandom((unsigned)time(NULL));
+
+        zmq::context_t context{1};
+        zmq::socket_t worker{context, ZMQ_DEALER};
+#if (defined (WIN32))
+            s_set_id(worker, (intptr_t)args);
+#else
+            s_set_id(worker); // set a printable identity
+#endif
+        worker.connect("tcp://localhost:5566");
+
+        while (true) {
+            // Tell the broker we're ready for work.
+            s_sendmore(worker, "");
+            s_send(worker, std::string{"Hey server"});
+            // Get workload from broker, until finished.
+            const std::string delimiter = s_recv(worker);
+            std::clog << "Envelope delimiter: \"" << delimiter << "\"\n";
+            const std::string workload = s_recv(worker);
+            std::clog << "Workload: " << workload << std::endl;
+
+            s_sleep(within(500) + 1);
         }
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
--- server/main.cpp
+++ server/main.cpp
@@ -1,12 +1,12 @@
 #include <zhelpers.hpp>
 
 #include <thread>
-
+#if 0
 int main()
 {
     try {
         zmq::context_t context{1};
-        zmq::socket_t publisher(context, ZMQ_PUB);
+        zmq::socket_t publisher{context, ZMQ_PUB};
         publisher.bind("tcp://127.0.0.1:5555");
 
         int i = 0;
@@ -22,6 +22,31 @@ int main()
             s_send(publisher, message);
 
             std::this_thread::sleep_for(std::chrono::seconds{1});
+        }
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
+#endif
+int main()
+{
+    try {
+        zmq::context_t context{1};
+        zmq::socket_t broker{context, ZMQ_ROUTER};
+        broker.bind("tcp://127.0.0.1:5566");
+
+        while (true) {
+            const std::string identity = s_recv(broker);
+            std::clog << "Identity: " << identity << std::endl;
+            const std::string delimiter = s_recv(broker);
+            std::clog << "Envelope delimiter: \"" << delimiter << "\"\n";
+            const std::string workerMessage = s_recv(broker);
+            std::clog << "Worker message: " << workerMessage << std::endl;
+            s_sendmore(broker, identity);
+            s_sendmore(broker, "");
+            s_send(broker, std::string{"Hi client"});
         }
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
