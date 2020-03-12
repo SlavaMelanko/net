96327968d81b9c9116b3f9b8cf3fa1ea8cb65b0e
smela <slava.melanko@gmail.com>
2020-03-12 21:34:31 +0200

    Updated project structure

--- publisher/samples/simple/main.cpp
+++ publisher/samples/simple/main.cpp
@@ -1,13 +1,24 @@
-#include "TcpServer.h"
+#include "ZmqPublisher.h"
 
 #include <Log.h>
+
+#include <zmq.hpp>
+
+#include <thread>
 
 int main()
 {
   InitLogging();
 
   try {
-    net::TcpServer{}.run();
+    zmq::context_t m_context{ 1 };
+    std::unique_ptr<net::Publisher> m_publisher{ std::make_unique<net::ZmqPublisher>(m_context, "127.0.0.1", 5555) };
+    int i = 0;
+    while (true) {
+      const std::string message{ "Message #" + std::to_string(++i) };
+      m_publisher->broadcast(message);
+      std::this_thread::sleep_for(std::chrono::seconds{ 1 });
+    }
   } catch (const std::exception& e) {
     ERROR(e.what());
   }
