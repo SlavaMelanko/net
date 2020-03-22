b9882eed1e262bddd869dcbed9e58257a2e3355c
smela <slava.melanko@gmail.com>
2020-03-22 20:24:58 +0200

    Added Publisher tests

---
+++ publisher/tests/PublisherTests.cpp
@@ -0,0 +1,34 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <ZmqPublisher.h>
+using namespace net;
+
+#include <memory>
+
+class PublisherTest : public testing::Test
+{
+protected:
+  void SetUp() { publisher = std::make_unique<ZmqPublisher>(context, "127.0.0.1", 5555); }
+  void TearDown() {}
+
+  zmq::context_t context{ 1 };
+  std::unique_ptr<Publisher> publisher;
+};
+
+TEST_F(PublisherTest, InitializeInstance)
+{
+  EXPECT_TRUE(context);
+}
+
+TEST_F(PublisherTest, SubscribeToTopic)
+{
+  EXPECT_TRUE(context);
+  EXPECT_TRUE(publisher->sendOut("notification", "hey there"));
+}
+
+TEST_F(PublisherTest, SubscribeToAllTopic)
+{
+  EXPECT_TRUE(context);
+  EXPECT_TRUE(publisher->broadcast("hey there"));
+}
