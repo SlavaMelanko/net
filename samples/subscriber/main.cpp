e1228d2e63f3c07493fd4295dde7f24e14dd9975
smela <slava.melanko@gmail.com>
2020-03-30 09:10:30 +0300

    Added comment into subscriber sample [skip ci]

--- samples/subscriber/main.cpp
+++ samples/subscriber/main.cpp
@@ -8,6 +8,13 @@ int main()
 {
   utils::InitLogging();
 
+  /**
+   Please note, even if you start a subscriber, wait a while, and then start the publisher, the
+   subscriber will always miss the first messages that the publisher sends. This is because as the
+   subscriber connects to the publisher (something that takes a small but non-zero time), the
+   publisher may already be sending messages out.
+   */
+
   try {
     zmq::context_t context{ 1 };
     std::unique_ptr<net::Subscriber> subscriber =
