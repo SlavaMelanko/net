049e0524f3abbdde4114147860f10289e4c08038
smela <slava.melanko@gmail.com>
2020-04-15 10:14:35 +0300

    Added command line parser to client sample

--- samples/client/main.cpp
+++ samples/client/main.cpp
@@ -4,31 +4,44 @@
 
 #include <zmq.hpp>
 
+#include <CLI/CLI.hpp>
+
 #include <thread>
 
-net::ConnectionSettings GetDefaultConnectionSettings()
+net::ConnectionSettings MakeConnectionSettings(const std::string& id, const uint32_t port)
 {
   net::ConnectionSettings connectionSettings;
-  connectionSettings.port = 5555;
+  if (!id.empty())
+    connectionSettings.id = id;
+  connectionSettings.port = port;
 
   return connectionSettings;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
   utils::Log::initialize();
+
+  CLI::App app{ "Client sample" };
+
+  std::string id;
+  app.add_option("-i,--id", id);
+  uint32_t port{ 0 };
+  app.add_option("-p,--port", port)->check(CLI::PositiveNumber);
+
+  CLI11_PARSE(app, argc, argv);
 
   try {
     zmq::context_t context{ 1 };
     net::ClientUnPtr client =
-      std::make_unique<net::ZmqClient>(context, GetDefaultConnectionSettings());
+      std::make_unique<net::ZmqClient>(context, MakeConnectionSettings(id, port));
 
     int i = 0;
     while (true) {
       const auto message{ "Message #" + std::to_string(++i) };
       const auto response = client->send(message);
-      utils::Log::info("Server responded: \"{}\"", response);
-      std::this_thread::sleep_for(std::chrono::seconds{ 1 });
+      utils::Log::info("Response: \"{}\"", response);
+      std::this_thread::sleep_for(std::chrono::milliseconds{ 250 });
     }
   } catch (const std::exception& e) {
     utils::Log::error(e.what());
