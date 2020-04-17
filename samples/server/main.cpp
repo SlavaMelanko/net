995c29afb098154f72329d49833e862dbb4e83e1
smela <slava.melanko@gmail.com>
2020-04-17 10:12:53 +0300

    Server sample: parsed command line args

--- samples/server/main.cpp
+++ samples/server/main.cpp
@@ -1,16 +1,25 @@
-#include "ZmqServer.h"
+#include <ZmqServer.h>
 
 #include <Log.h>
 
 #include <zmq.hpp>
 
-int main()
+#include <CLI/CLI.hpp>
+
+int main(int argc, char* argv[])
 {
   utils::Log::initialize();
 
   try {
+    CLI::App app{ "Server sample" };
+
+    uint32_t port{ 0 };
+    app.add_option("-p,--port", port)->check(CLI::PositiveNumber);
+
+    CLI11_PARSE(app, argc, argv);
+
     zmq::context_t context{ 1 };
-    net::ServerUnPtr server = std::make_unique<net::ZmqServer>(context, "127.0.0.1", 5555);
+    net::ServerUnPtr server = std::make_unique<net::ZmqServer>(context, "127.0.0.1", port);
     while (true) {
       server->run();
     }
