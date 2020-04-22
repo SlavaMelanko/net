b9bc08946afeab2a2bd77103d3b489e42f075c80
smela <slava.melanko@gmail.com>
2020-04-22 09:37:42 +0300

    Added command line parser to pub/sub

--- samples/publisher/main.cpp
+++ samples/publisher/main.cpp
@@ -1,18 +1,27 @@
 #include <Log.h>
 #include <ZmqPublisher.h>
 
-#include <zmq.hpp>
+#include <CLI/CLI.hpp>
 
 #include <thread>
 
-int main()
+int main(int argc, char* argv[])
 {
   utils::Log::initialize();
 
   try {
+    CLI::App app{ "Publisher sample" };
+
+    std::string host{ "127.0.0.1" };
+    app.add_option("-o,--host", host)->check(CLI::ValidIPV4);
+    uint32_t port{ 0 };
+    app.add_option("-p,--port", port)->check(CLI::PositiveNumber);
+
+    CLI11_PARSE(app, argc, argv);
+
     zmq::context_t context{ 1 };
-    std::unique_ptr<net::Publisher> publisher{ std::make_unique<net::ZmqPublisher>(
-      context, "127.0.0.1", 5555) };
+    net::PublisherUnPtr publisher = std::make_unique<net::ZmqPublisher>(context, host, port);
+
     int i = 0;
     while (true) {
       const std::string message{ "Message #" + std::to_string(++i) };
--- samples/subscriber/main.cpp
+++ samples/subscriber/main.cpp
@@ -1,23 +1,31 @@
 #include <Log.h>
 #include <ZmqSubscriber.h>
 
-#include <memory>
+#include <CLI/CLI.hpp>
 
-int main()
+/**
+ * Please note, even if you start a subscriber, wait a while, and then start the publisher,
+ * the subscriber will always miss the first messages that the publisher sends. This is because as
+ * the subscriber connects to the publisher (something that takes a small but non-zero time),
+ * the publisher may already be sending messages out.
+ */
+
+int main(int argc, char* argv[])
 {
   utils::Log::initialize();
 
-  /**
-   Please note, even if you start a subscriber, wait a while, and then start the publisher, the
-   subscriber will always miss the first messages that the publisher sends. This is because as the
-   subscriber connects to the publisher (something that takes a small but non-zero time), the
-   publisher may already be sending messages out.
-   */
-
   try {
+    CLI::App app{ "Subscriber sample" };
+
+    std::string host{ "127.0.0.1" };
+    app.add_option("-o,--host", host)->check(CLI::ValidIPV4);
+    uint32_t port{ 0 };
+    app.add_option("-p,--port", port)->check(CLI::PositiveNumber);
+
+    CLI11_PARSE(app, argc, argv);
+
     zmq::context_t context{ 1 };
-    std::unique_ptr<net::Subscriber> subscriber =
-      std::make_unique<net::ZmqSubscriber>(context, "127.0.0.1", 5555);
+    net::SubscriberUnPtr subscriber = std::make_unique<net::ZmqSubscriber>(context, host, port);
     subscriber->subscribeToAllTopics();
 
     while (true) {
