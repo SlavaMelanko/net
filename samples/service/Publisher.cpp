596eaf5b2d81c5092aa9fb5aaf664698331acf4c
smela <slava.melanko@gmail.com>
2020-06-06 09:45:14 +0300

    Update publisher using BlockingConcurrentQueue

--- samples/service/Publisher.cpp
+++ samples/service/Publisher.cpp
@@ -5,28 +5,48 @@
 #include <Log.h>
 
 Publisher::Publisher(zmq::context_t& context, std::string_view host, const uint16_t port)
-  : m_publisher{ std::make_unique<net::ZmqPublisher>(context, host, port) }
+  : m_queue{}
+  , m_publisher{ std::make_unique<net::ZmqPublisher>(context, host, port) }
 {}
 
 Publisher::~Publisher()
 {
+  m_running = false;
+
+  if (m_queue.size_approx() == 0)
+    m_queue.enqueue({});
+
   if (m_thread.joinable())
     m_thread.join();
 }
 
 void Publisher::run()
 {
+  if (m_running) return;
+
+  m_running = true;
+
   m_thread = std::thread{ &Publisher::process, this };
+}
+
+bool Publisher::push(std::string_view message, std::string_view topic)
+{
+  return m_queue.enqueue({ topic.data(), message.data() });
 }
 
 void Publisher::process()
 {
-  int i = 0;
-  while (true) {
+  while (m_running) {
     try {
-      const std::string message{ "Message #" + std::to_string(++i) };
-      m_publisher->broadcast(message);
-      std::this_thread::sleep_for(std::chrono::seconds{ 2 });
+      Notification notification;
+      m_queue.wait_dequeue(notification);
+
+      if (!m_running) break;
+
+      if (notification.topic.empty())
+        m_publisher->broadcast(notification.message);
+      else
+        m_publisher->sendOut(notification.topic, notification.message);
     } catch (zmq::error_t& e) {
       net::Log::error(e.what());
       if (e.num() == ETERM)
--- samples/service/Publisher.h
+++ samples/service/Publisher.h
@@ -2,6 +2,8 @@
 
 #include <zmq.hpp>
 
+#include <moodycamel/blockingconcurrentqueue.h>
+
 #include <thread>
 
 namespace net {
@@ -11,14 +13,33 @@ class Publisher;
 class Publisher
 {
 public:
+  struct Notification
+  {
+    std::string topic;
+    std::string message;
+  };
+
   Publisher(zmq::context_t& context, std::string_view host, const uint16_t port);
   ~Publisher();
 
   void run();
 
+  /**
+   * @brief Enqueues notification message.
+   *
+   * @param message Notification data to be sent.
+   * @param topic Topic name where to send out a notification message.
+   *              If it is empty - notification is broadcasted to all subscribers.
+   *
+   * @return true if notification message has been enqueued successfully, otherwise - false.
+   */
+  bool push(std::string_view message, std::string_view topic = "");
+
 private:
   void process();
 
+  std::atomic_bool m_running{ false };
+  moodycamel::BlockingConcurrentQueue<Notification> m_queue;
   std::unique_ptr<net::Publisher> m_publisher;
   std::thread m_thread;
 };
