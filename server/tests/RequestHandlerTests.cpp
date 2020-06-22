ec1898c6455eac2bdf9ef784f3612f6f680dfd68
Slava <slava.melanko@gmail.com>
2020-06-22 09:45:08 +0300

    Added simpe test for heartbeat handler

---
+++ server/tests/RequestHandlerTests.cpp
@@ -0,0 +1,12 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <HeartbeatRequestHandler.h>
+
+TEST(HeartbeatRequestHandlerTest, CheckResponse)
+{
+  net::HeartbeatRequestHandler heartbeat;
+  const auto response = heartbeat.handle({});
+  EXPECT_EQ(response.getString("srv_v").value(), "0.0.1");
+  EXPECT_EQ(response.getString("zmq_v").value(), "4.3.2");
+}
--- server/tests/ServerTests.cpp
+++ server/tests/ServerTests.cpp
@@ -2,11 +2,10 @@
 #include <gtest/gtest.h>
 
 #include <ZmqServer.h>
-using namespace net;
 
 TEST(ServerTest, InitializeInstance)
 {
   zmq::context_t context{ 1 };
   EXPECT_TRUE(context);
-  EXPECT_NO_THROW(std::make_unique<ZmqServer>(context, "127.0.0.1", 5555));
+  EXPECT_NO_THROW(std::make_unique<net::ZmqServer>(context, "127.0.0.1", 5555));
 }
