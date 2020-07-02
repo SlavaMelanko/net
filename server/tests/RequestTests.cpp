a06c812585b729b8b25311ea21e94fdf7b8d1f2c
Slava <slava.melanko@gmail.com>
2020-07-02 10:33:45 +0300

    Added test

---
+++ server/tests/RequestTests.cpp
@@ -0,0 +1,40 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <Request.h>
+
+#include <type_traits>
+
+TEST(RequestTest, CheckTypeTraits)
+{
+  using Type = net::Request;
+
+  EXPECT_TRUE(std::is_class_v<Type>);
+
+  EXPECT_TRUE(std::is_object_v<Type>);
+
+  EXPECT_FALSE(std::is_trivially_copyable_v<Type>);
+
+  EXPECT_TRUE(std::is_standard_layout_v<Type>);
+
+  EXPECT_TRUE(std::is_default_constructible_v<Type>);
+  EXPECT_FALSE(std::is_trivially_default_constructible_v<Type>);
+  EXPECT_FALSE(std::is_nothrow_default_constructible_v<Type>);
+
+  EXPECT_FALSE(std::is_copy_constructible_v<Type>);
+  EXPECT_FALSE(std::is_copy_assignable_v<Type>);
+
+  EXPECT_TRUE(std::is_move_constructible_v<Type>);
+  EXPECT_FALSE(std::is_trivially_move_constructible_v<Type>);
+  EXPECT_TRUE(std::is_nothrow_move_constructible_v<Type>);
+
+  EXPECT_TRUE(std::is_move_assignable_v<Type>);
+  EXPECT_FALSE(std::is_trivially_move_assignable_v<Type>);
+  EXPECT_TRUE(std::is_nothrow_move_assignable_v<Type>);
+
+  EXPECT_TRUE(std::is_destructible_v<Type>);
+  EXPECT_TRUE(std::is_nothrow_destructible_v<Type>);
+  EXPECT_FALSE(std::is_trivially_destructible_v<Type>);
+
+  EXPECT_FALSE(std::has_virtual_destructor_v<Type>);
+}
