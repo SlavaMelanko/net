82530d58fb283355eb0ab1883b855c5b811c706e
Slava <slava.melanko@gmail.com>
2020-07-04 10:02:27 +0300

    Update RequestTests.cpp

--- server/tests/RequestTests.cpp
+++ server/tests/RequestTests.cpp
@@ -9,19 +9,25 @@ TEST(RequestTest, CheckTypeTraits)
 {
   using Type = net::Request;
 
+  // Primary type categories.
   EXPECT_TRUE(std::is_class_v<Type>);
 
+  // Composite type categories.
   EXPECT_TRUE(std::is_object_v<Type>);
 
+  // Type properties.
   EXPECT_FALSE(std::is_trivially_copyable_v<Type>);
-
 #if defined(__APPLE__) || defined(__linux__)
   EXPECT_TRUE(std::is_standard_layout_v<Type>);
 #elif defined(_WIN32)
   // AppVeyor: true for release, and false for debug?
   //EXPECT_FALSE(std::is_standard_layout_v<Type>);
 #endif
+  EXPECT_FALSE(std::is_abstract_v<Type>);
+  EXPECT_FALSE(std::is_polymorphic_v<Type>);
+  EXPECT_FALSE(std::is_final_v<Type>);
 
+  // Supported operations.
   EXPECT_TRUE(std::is_default_constructible_v<Type>);
   EXPECT_FALSE(std::is_trivially_default_constructible_v<Type>);
   EXPECT_FALSE(std::is_nothrow_default_constructible_v<Type>);
