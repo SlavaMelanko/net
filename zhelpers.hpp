9d3fbaf0d0e3f3114b297e381194741598e9a077
Slava <slava.melanko@gmail.com>
2022-06-01 17:27:11 +0300

    Removed broker and worker

--- zhelpers.hpp
+++ zhelpers.hpp
@@ -108,7 +108,8 @@ inline static void s_dump(zmq::socket_t& socket)
     int more = 0; //  Multipart detection
     size_t more_size = sizeof(more);
     socket.getsockopt(ZMQ_RCVMORE, &more, &more_size);
-    if (!more) break; // last message part
+    if (!more)
+      break; //  Last message part
   }
 }
 
