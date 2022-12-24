487d255ab2f9eea4330535771685eb47818abbd2
Slava Melanko <slava.melanko@gmail.com>
2022-12-24 13:12:09 +0200

    Fixed code formatting

--- ui-sciter/main.cpp
+++ ui-sciter/main.cpp
@@ -1,9 +1,13 @@
 #include "sciter-x-window.hpp"
 #include "sciter-x.h"
 
-class MainWindow : public sciter::window {
+class MainWindow : public sciter::window
+{
 public:
-  MainWindow() : window(SW_MAIN | SW_ENABLE_DEBUG | SW_CONTROLS) {}
+  MainWindow()
+    : window(SW_MAIN | SW_ENABLE_DEBUG | SW_CONTROLS)
+  {
+  }
 
   // passport - lists native functions and properties exposed to script under
   // 'MainWindow' interface name:
@@ -18,7 +22,8 @@ public:
 
 #include "sciter-resources.cpp" // resources packaged into binary blob.
 
-int uimain(std::function<int()> run) {
+int uimain(std::function<int()> run)
+{
   // Bind resources[] (defined in "sciter-resources.cpp") with the archive.
   sciter::archive::instance().open(aux::elements_of(resources));
 
